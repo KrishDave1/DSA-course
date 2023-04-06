2edcc84e3429bf6333d0fdf6e0498a4fd29b2514
KrishDave1 <116882111+KrishDave1@users.noreply.github.com>
2023-04-06 09:36:00 +0530

    Add files via upload

---
+++ BinarySearch/last_occ_BS.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_search1(vector<int> v, int n)
+{
+    int high = v.size() - 1, low = 0;
+    int mid = 0, res = -1;
+    while (high >= low)
+    {
+        mid = (low + high) / 2;
+        if (v[mid] == n)
+        {
+            res = mid;
+            low = mid + 1;
+        }
+        else if (v[mid] > n)
+        {
+            high = mid - 1;
+        }
+        else if (v[mid] < n)
+        {
+            low = mid + 1;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    vector<int> v = {10, 50, 50, 50, 50, 60};
+    int n = 50;
+    cout << binary_search1(v, n) << endl;
+}
